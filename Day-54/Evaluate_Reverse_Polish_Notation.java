6aa8a7d100ca7576a8fbc2ce5af65ea2d9b785fd
Abhmanyu Singh <abhimanyu.24bce10726@vitbhopal.ac.in>
2026-05-09 09:44:18 +0530

    Create Evaluate_Reverse_Polish_Notation.java

---
+++ Day-54/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String token : tokens) {
+
+            switch (token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+
+                case "-": {
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a - b);
+                    break;
+                }
+
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+
+                case "/": {
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a / b); // truncates toward 0
+                    break;
+                }
+
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.peek();
+    }
+}
