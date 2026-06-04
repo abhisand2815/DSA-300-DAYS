c561c5bc4a0f987b2ac7e6d02286fb6b45ddc217
Abhmanyu Singh <abhimanyu.24bce10726@vitbhopal.ac.in>
2026-06-04 09:38:21 +0530

    Create Lowest_Common_Ancestor_of_a_Binary_Tree.java

---
+++ Day-80/Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // base case
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // if both sides found → this is LCA
+        if (left != null && right != null) {
+            return root;
+        }
+
+        // otherwise return non-null side
+        return (left != null) ? left : right;
+    }
+}
