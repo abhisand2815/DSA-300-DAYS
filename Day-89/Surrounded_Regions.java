c21ad3403daa393a186b17d4a2445b821f0b3a34
Abhmanyu Singh <abhimanyu.24bce10726@vitbhopal.ac.in>
2026-06-13 17:29:21 +0530

    Create Surrounded_Regions.java

---
+++ Day-89/Surrounded_Regions.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        // Step 1: mark border connected 'O'
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        // Step 2: flip & restore
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X'; // captured
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O'; // restore
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length
+            || board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = '#';
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
